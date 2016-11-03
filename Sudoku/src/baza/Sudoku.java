143597f5add9fdd110cbc85481cffb0076c8c002
miladavuk <milada.vukovic@gmail.com>
2016-11-03 21:31:52 +0100

    Uradjena klasa Matrice i pripremljena klasa Sudoku

---
+++ Sudoku/src/baza/Sudoku.java
@@ -0,0 +1,17 @@
+package baza;
+
+public class Sudoku {
+	
+	
+	char [][] vratiRandomMatricu(){
+		
+		//ispisi metodu
+		return null;
+	}
+	
+	char [][] vratiMatricuZaPrikaz(char [][] randomMatrica){
+		
+		return null;
+	}
+
+}
